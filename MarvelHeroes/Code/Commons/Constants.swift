1098e6c0f5fb5721177c6f09a9c82e21e817372c
José María Ila <joseila85@gmail.com>
2017-08-08 18:49:51 +0200

    Set base configuration

---
+++ MarvelHeroes/Code/Commons/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MarvelHeroes
+//
+//  Created by José María Ila on 8/8/17.
+//  Copyright © 2017 José María Ila. All rights reserved.
+//
+
+import Foundation
+
+struct MarvelAPI {
+    static var heroesList: String {
+        return "https://api.myjson.com/bins/bvyob"
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+
+use_frameworks!
+
+target 'MarvelHeroes' do
+
+    pod 'Alamofire', '~> 4.4'
+    pod 'AlamofireObjectMapper', '~> 4.0'
+    pod 'AlamofireImage', '~> 3.1'
+
+end
